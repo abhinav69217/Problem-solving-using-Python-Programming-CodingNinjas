9d340ea9e5d61d28583127c7d9236484aee5976f
Abhinav <91418569+abhinav69217@users.noreply.github.com>
2021-09-27 18:54:20 +0530

    Update Diamond of stars.py

--- Patterns2/Diamond of stars.py
+++ Patterns2/Diamond of stars.py
@@ -1,3 +1,38 @@
+Print the following pattern for the given number of rows.
+Note: N is always odd.
+
+
+Pattern for N = 5
+
+
+
+The dots represent spaces.
+
+
+
+Input format :
+N (Total no. of rows and can only be odd)
+Output format :
+Pattern in N lines
+Constraints :
+1 <= N <= 49
+Sample Input 1:
+5
+Sample Output 1:
+  *
+ ***
+*****
+ ***
+  *
+Sample Input 2:
+3
+Sample Output 2:
+  *
+ ***
+  *
+
+
+
 n = int(input())
 upper_part = (n+1)//2
 lower_part = n // 2
